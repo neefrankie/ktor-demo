ef529ddfb05a95a3a0acf5eb1e207f210a3e2f52
weiguo.ni <neefrankie@gmail.com>
2022-09-22 17:50:38 +0800

    add db and json support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,8 @@ dependencies {
 
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
@@ -47,6 +49,7 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("mysql:mysql-connector-java:8.0.30")
 
     // This dependency is used by the application.
     implementation("com.google.guava:guava:31.0.1-jre")
