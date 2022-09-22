88ef6fece56d702742ff03c52518fab3a39405b0
weiguo.ni <neefrankie@gmail.com>
2022-09-22 14:39:18 +0800

    add exposed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,6 +9,7 @@
 
 val ktor_version: String by project
 val logback_version: String by project
+val exposed_version: String by project
 
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
@@ -42,6 +43,10 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
+
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
 
     // This dependency is used by the application.
     implementation("com.google.guava:guava:31.0.1-jre")
