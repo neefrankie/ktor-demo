ed7fe478cccd180491812ba94d6aba3c86763672
weiguo.ni <neefrankie@gmail.com>
2022-09-23 13:56:05 +0800

    addLogger

--- app/src/main/kotlin/com/ftchinese/api/dao/DatabaseFactory.kt
+++ app/src/main/kotlin/com/ftchinese/api/dao/DatabaseFactory.kt
@@ -1,24 +1,29 @@
 package com.ftchinese.api.dao
 
-import com.ftchinese.api.models.Stories
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
 import kotlinx.coroutines.Dispatchers
 import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.StdOutSqlLogger
+import org.jetbrains.exposed.sql.addLogger
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import org.jetbrains.exposed.sql.transactions.transaction
 
 object DatabaseFactory {
+    private lateinit var contentDb: Database
     fun init() {
-        val database = Database.connect(
-            url = "jdbc:mysql://localhost:3306/cmstmp01",
-            driver = "com.mysql.cj.jdbc.Driver",
-            user = "sampadm",
+        val config = HikariConfig().apply {
+            jdbcUrl = "jdbc:mysql://localhost:3306/cmstmp01"
+            driverClassName = "com.mysql.cj.jdbc.Driver"
+            username = "sampadm"
             password = "secret"
-        )
-        transaction(database) {
-            SchemaUtils.create(Stories)
+        }
+        val dataSource = HikariDataSource(config)
+        contentDb = Database.connect(dataSource)
+        transaction(contentDb) {
+            addLogger(StdOutSqlLogger)
         }
     }
 
-    suspend fun <T> dbQuery(block: suspend () -> T): T = newSuspendedTransaction(Dispatchers.IO) { block() }
+    suspend fun <T> dbQuery(block: suspend () -> T): T = newSuspendedTransaction(Dispatchers.IO, contentDb) { block() }
 }
