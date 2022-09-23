daff243b8d175e61a5a458eb918864aeb81c9d8e
weiguo.ni <neefrankie@gmail.com>
2022-09-23 11:00:13 +0800

    accessRight changed to bool due to strange db schema

--- app/src/main/kotlin/com/ftchinese/api/models/RawStory.kt
+++ app/src/main/kotlin/com/ftchinese/api/models/RawStory.kt
@@ -6,25 +6,26 @@ data class RawStory(
     val id: String,
     val createdAt: Long,
     val updatedAt: Long,
-    val accessRight: Int,
+    val accessRight: Boolean,
     val titleCn: String,
     val titleEn: String,
     val longLeadCn: String,
     val tag: String,
+    val area: String,
+    val genre: String,
+    val topic: String,
+    val industry: String,
+
     val bylineDescCn: String,
     val bylineDescEn: String,
     val bylineAuthorCn: String,
     val bylineAuthorEn: String,
     val bylineStatusCn: String,
     val bylineStatusEn: String,
-    val genre: String,
-    val topic: String,
-    val industry: String,
-    val area: String,
     val bodyCn: String,
     val bodyEn: String,
 ) {
-    val isBilingual: Boolean
+    private val isBilingual: Boolean
         get() = bodyCn != "" && bodyEn != ""
 
     fun build(): Story {
@@ -33,19 +34,15 @@ data class RawStory(
             kind = ContentKind.Story,
             createdAt = createdAt,
             updatedAt = updatedAt,
-            tier = when (accessRight) {
-                1 -> Tier.Standard
-                2 -> Tier.Premium
-                else -> null
-            },
+            tier = if (accessRight) Tier.Standard else null,
             title = titleCn,
             standfirst = longLeadCn,
-            tags = tag.split(","),
+            tags = tag,
             bilingual = isBilingual,
-            areas = area.split(","),
-            genre = genre.split(","),
-            industries = industry.split(","),
-            topics = topic.split(","),
+            regions = area,
+            genres = genre,
+            industries = industry,
+            topics = topic,
             contentCn = StoryDetails(
                 title = titleCn,
                 byline = Byline(
@@ -71,7 +68,7 @@ object Stories : Table(name = "story") {
     val id = varchar("id", 20)
     val createdAt = long("fileupdatetime")
     val updatedAt = long("last_publish_time")
-    val accessRight = integer("access_right")
+    val accessRight = bool("accessright")
     val longLeadCn = varchar("clongleadbody", 2000)
     val tag = varchar("tag", 200)
     val titleCn = varchar("cheadline", 255)
