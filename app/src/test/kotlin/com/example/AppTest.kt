aeb219e194488861e2d96d060fa9e8b9e1bd6590
weiguo.ni <neefrankie@gmail.com>
2022-09-29 16:49:07 +0800

    fix

--- app/src/test/kotlin/com/example/AppTest.kt
+++ app/src/test/kotlin/com/example/AppTest.kt
@@ -3,6 +3,7 @@
  */
 package com.example
 
+import com.example.plugins.configureApiRouting
 import com.example.plugins.configureArticleRouting
 import com.example.plugins.configureSerialization
 import io.ktor.client.request.*
@@ -16,10 +17,10 @@ class OrderRouteTests {
     @Test
     fun testGetOrder() = testApplication {
         application {
-            configureArticleRouting()
+            configureApiRouting()
             configureSerialization()
         }
-        val response = client.get("/order/2020-04-06-01")
+        val response = client.get("/api/order/2020-04-06-01")
         assertEquals(
             """{"number":"2020-04-06-01","contents":[{"item":"Ham Sandwich","amount":2,"price":5.5},{"item":"Water","amount":1,"price":1.5},{"item":"Beer","amount":3,"price":2.3},{"item":"Cheesecake","amount":1,"price":3.75}]}""".trimIndent(),
             response.bodyAsText()
