027a24ebb552d09e04af8e139a8c366c1ccbdf70
weiguo.ni <neefrankie@gmail.com>
2022-09-29 16:49:22 +0800

    refactor

--- app/src/main/kotlin/com/example/routes/ArticleRoutes.kt
+++ app/src/main/kotlin/com/example/routes/ArticleRoutes.kt
@@ -1,9 +1,12 @@
 package com.example.routes
 
 import com.example.dao.dao
+import com.example.models.Article
+import com.example.models.ArticleForm
 import com.example.pages.*
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
 import io.ktor.server.html.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
@@ -19,20 +22,6 @@ fun Route.articleRouting() {
                 articleListPage(articles)
             }
         }
-        get("new") {
-            // Show a page with fields for creating a new article
-            call.respondHtml(status = HttpStatusCode.OK) {
-                newArticlePage()
-            }
-        }
-        post {
-            // save an article
-            val formParameters = call.receiveParameters()
-            val title = formParameters.getOrFail("title")
-            val body = formParameters.getOrFail("body")
-            val article = dao.addNewArticle(title, body)
-            call.respondRedirect("/articles/${article?.id}")
-        }
         get("{id}") {
             // Show an article with a specific id
             val id = call.parameters.getOrFail<Int>("id").toInt()
@@ -45,32 +34,74 @@ fun Route.articleRouting() {
                 }
             }
         }
-        get("{id}/edit") {
-            // Show a page with fields for editing an article
-            val id = call.parameters.getOrFail<Int>("id").toInt()
-            val article = dao.article(id)
-            call.respondHtml {
-                if (article != null) {
-                    articleEditPage(article)
-                } else {
-                    notFoundPage()
+        authenticate("auth-session") {
+            get("new") {
+                // Show a page with fields for creating a new article
+                call.respondHtml(status = HttpStatusCode.OK) {
+                    newArticlePage()
                 }
             }
-        }
-        post("{id}") {
-            // Update or delete an article
-            val id = call.parameters.getOrFail<Int>("id").toInt()
-            val formParameters = call.receiveParameters()
-            when (formParameters.getOrFail("_action")) {
-                "update" -> {
-                    val title = formParameters.getOrFail("title")
-                    val body = formParameters.getOrFail("body")
-                    dao.editArticle(id, title, body)
-                    call.respondRedirect("/articles/$id")
+            post {
+                // save an article
+                val formParameters = call.receiveParameters()
+                val v = ArticleForm(
+                    title = formParameters.getOrFail("title"),
+                    body = formParameters.getOrFail("body")
+                )
+                val err = v.validate()
+                if (err.isEmpty()) {
+                    val article = dao.addNewArticle(v.title, v.body)
+                    call.respondRedirect("/articles/${article?.id}")
+                } else {
+                    call.respondHtml(status = HttpStatusCode.OK) {
+                        newArticlePage(
+                            fieldErr = err,
+                            value = v
+                        )
+                    }
                 }
-                "delete" -> {
-                    dao.deleteArticle(id)
-                    call.respondRedirect("/articles")
+            }
+
+            get("{id}/edit") {
+                // Show a page with fields for editing an article
+                val id = call.parameters.getOrFail<Int>("id").toInt()
+                val article = dao.article(id)
+                call.respondHtml {
+                    if (article != null) {
+                        articleEditPage(article)
+                    } else {
+                        notFoundPage()
+                    }
+                }
+            }
+            post("{id}") {
+                // Update or delete an article
+                val id = call.parameters.getOrFail<Int>("id").toInt()
+                val formParameters = call.receiveParameters()
+                when (formParameters.getOrFail("_action")) {
+                    "update" -> {
+                        val v = ArticleForm(
+                            title = formParameters.getOrFail("title"),
+                            body = formParameters.getOrFail("body")
+                        )
+                        val err = v.validate()
+                        if (err.isEmpty()) {
+                            dao.editArticle(id, v.title, v.body)
+                            call.respondRedirect("/articles/$id")
+                        } else {
+                            call.respondHtml {
+                                articleEditPage(Article(
+                                    id = id,
+                                    title = v.title,
+                                    body = v.body
+                                ))
+                            }
+                        }
+                    }
+                    "delete" -> {
+                        dao.deleteArticle(id)
+                        call.respondRedirect("/articles")
+                    }
                 }
             }
         }
